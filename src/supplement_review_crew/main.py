079b729bba57f2837af926c1b7ffed46c7a5e51b
Arwan Ahmad Khoiruddin <arwan.khoiruddin@gmail.com>
2024-04-18 15:48:02 +0700

    Restructure so we can run different crew for different case

--- src/supplement_review_crew/main.py
+++ src/supplement_review_crew/main.py
@@ -4,11 +4,15 @@ from supplement_review_crew.crew import SupplementReviewCrew
 load_dotenv()
 
 
-def run():
+def supplement_review():
     inputs = {
         'topic': 'Pineal XT',
     }
     SupplementReviewCrew().crew().kickoff(inputs=inputs)
+
+
+def run():
+    supplement_review()
 
 
 if __name__ == '--main__':
